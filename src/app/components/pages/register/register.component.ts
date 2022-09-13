8e73dfb605cfe611f0211f9759b50685a82c851e
Dejhon <dejhonsherman6@gmail.com>
2022-09-13 13:05:14 -0500

    Api connect to the registration form

--- src/app/components/pages/register/register.component.ts
+++ src/app/components/pages/register/register.component.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { EmailValidator, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import Validation from 'src/app/directives/custom.validator';
+import { UserService } from 'src/app/Services/user.service';
+import { Users } from 'src/app/Models/Users';
 
 @Component({
   selector: 'app-register',
@@ -12,19 +14,33 @@ import Validation from 'src/app/directives/custom.validator';
 export class RegisterComponent implements OnInit {
 
   submitted = false;
+  users:Users[] = [];
+  emailMatch = false;
+
   registerForm = new FormGroup({
     'email': new FormControl('', [Validators.required, Validators.email]),
     'password': new FormControl('', [Validators.required]),
     'cpassword': new FormControl('', [Validators.required]),
+    'role': new FormControl('user', Validators.required)
   },
     { validators: [Validation.match('password', 'cpassword')] }
   )
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private userService: UserService, private router: Router) { }
 
-  loginIn() {
-    this.submitted = true;
-  }
-
+  register(body:object) {
+        if(this.registerForm?.valid){
+            this.userService.createUser(body).subscribe({
+             next:()=>{
+               alert('Registration Successful')
+             },
+             error:(err)=>{
+               console.log(err);        
+             }
+            })
+           }else{
+            alert('This form is invalid')
+           }
+        }        
 
   ngOnInit(): void {
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  BASE_URL:'http://localhost:8080/api/v1'
+  BASE_URL:'http://10.44.16.118:8080/api/v1'
 };
 
 /*
