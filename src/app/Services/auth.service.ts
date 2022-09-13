483f2e0884e76f316b4f26067b0f9aa6bd5b7952
Dejhon <dejhonsherman6@gmail.com>
2022-09-13 16:44:16 -0500

    login implemented

---
+++ src/app/Services/auth.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Users } from '../Models/Users';
+import { environment } from 'src/environments/environment';
+import { Observable, of, catchError, map, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  API_URL = environment.BASE_URL + '/auth/login'
+
+  constructor(private auth: HttpClient) { }
+
+  isLoggedIn(){
+    return !!localStorage.getItem('token')
+  }
+
+  loginUser(data:object):Observable<Users>{
+    return this.auth.post<Users>(`${this.API_URL}`, data).pipe(
+      tap( userLoggedIn => console.log(`${userLoggedIn}`)),
+      catchError( error => of())
+    );
+  }
+  
+}
