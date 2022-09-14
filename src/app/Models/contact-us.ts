a0cb76d002c05b9482f2b043e9a3db04c26ae16a
Dejhon <dejhonsherman6@gmail.com>
2022-09-14 09:17:54 -0500

    contact model created

---
+++ src/app/Models/contact-us.ts
@@ -0,0 +1,19 @@
+export class Users {
+    _id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    phoneNumber: number;
+    subject: string;
+    message: string;
+
+    constructor(id?: string, firstName?: string, lastName?: string, email?: string, phoneNumber?: number, subject?: string, message?: string){
+        this._id = id!;
+        this.firstName = firstName!;
+        this.lastName= lastName!;
+        this.email = email!;
+        this.phoneNumber = phoneNumber!;
+        this.subject = subject!;
+        this.message = message!;
+    }
+}
