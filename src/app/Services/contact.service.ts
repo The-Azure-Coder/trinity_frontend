2df3db1018b80148851a9abede6cf6c4110893e7
Roshane <39389600+Roshane-Johnson@users.noreply.github.com>
2022-09-15 09:52:39 -0500

    created api response interface and example of usage in Contact Service

--- src/app/Services/contact.service.ts
+++ src/app/Services/contact.service.ts
@@ -3,51 +3,50 @@ import { Injectable } from '@angular/core';
 import { Contact } from '../models/contact-us';
 import { environment } from 'src/environments/environment';
 import { Observable, of, catchError, map, tap } from 'rxjs';
-
+import { ApiResponse } from '../models/api-response';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContactService {
-
   API_URL = environment.BASE_URL + '/contact-us';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-
-  createMessage(data: object): Observable<Contact> {
-    return this.http.post<Contact>(`${this.API_URL}`, data).pipe(
-      tap(newMessage => console.log(`${newMessage}`)),
-      catchError(error => of())
+  createMessage(data: object): Observable<ApiResponse<Contact>> {
+    return this.http.post<ApiResponse<Contact>>(`${this.API_URL}`, data).pipe(
+      tap((newMessage: any) => console.log(`${newMessage}`)),
+      catchError((error) => of())
     );
   }
 
-  getMessages(): Observable<Contact[]> {
-    return this.http.get<Contact[]>(this.API_URL).pipe(
+  getMessages(): Observable<ApiResponse<Contact[]>> {
+    return this.http.get<ApiResponse<Contact>>(this.API_URL).pipe(
       tap((allMessages: any) => console.log(`${allMessages}`)),
-      catchError(error => of([])),
+      catchError((error) => of([]))
     );
   }
 
-  getMessageById(id: string): Observable<Contact | any> {
-    return this.http.get<Contact>(`${this.API_URL}/${id}`).pipe(
-      tap(selectedmessage => console.log(`${selectedmessage}`)),
-      catchError(error => of(new Contact())),
+  getMessageById(id: string): Observable<ApiResponse<Contact>> {
+    return this.http.get<ApiResponse<Contact>>(`${this.API_URL}/${id}`).pipe(
+      tap((selectedmessage: any) => console.log(`${selectedmessage}`)),
+      catchError((error) => of(new Contact()))
     );
   }
 
-  updatemessage(id: string, body:object): Observable<Contact>{
-    return this.http.patch<Contact>(`${this.API_URL}/${id}`, body).pipe(
-      tap(updatedRecord => console.log(`${updatedRecord}`)),
-      catchError(error => of(new Contact())),
-    );
+  updatemessage(id: string, body: object): Observable<ApiResponse<Contact>> {
+    return this.http
+      .patch<ApiResponse<Contact>>(`${this.API_URL}/${id}`, body)
+      .pipe(
+        tap((updatedRecord: any) => console.log(`${updatedRecord}`)),
+        catchError((error) => of(new Contact()))
+      );
   }
 
-  deleteMessage(id: string): Observable<Contact> {
-    return this.http.delete<Contact>(`${this.API_URL}/${id}`).pipe(
-      tap(deletedMessage => console.log(`${deletedMessage}`)),
-      catchError(error => of())
+  deleteMessage(id: string): Observable<ApiResponse<Contact>> {
+    return this.http.delete<ApiResponse<Contact>>(`${this.API_URL}/${id}`).pipe(
+      tap((deletedMessage: any) => console.log(`${deletedMessage}`)),
+      catchError((error) => of())
     );
   }
-
 }
