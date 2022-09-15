5b7fa9a1ea737530f63b79defa4b567ef1874d8b
Tyrese <jrackfinn@gmail.com>
2022-09-14 23:19:00 -0500

    service name change

--- src/app/Services/plumbing-services.service.ts
+++ src/app/Services/plumbing-services.service.ts
@@ -1,50 +1,50 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Services } from '../Models/service';
-import { environment } from 'src/environments/environment';
+import { Services } from '../models/service';
+import { environment } from '../../environments/environment';
 import { Observable, of, catchError, map, tap } from 'rxjs';
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class ServicesService {
+export class PlumbingServicesService {
   API_URL = environment.BASE_URL + '/services'
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  createService(data:object):Observable<Services>{
+  createService(data: object): Observable<Services> {
     return this.http.post<Services>(`${this.API_URL}`, data).pipe(
-      tap( newService => console.log(`${newService}`)),
-      catchError( error => of())
+      tap(newService => console.log(`${newService}`)),
+      catchError(error => of())
     );
   }
 
-  getServices(): Observable<Services[]>{
+  getServices(): Observable<Services[]> {
     return this.http.get<Services[]>(this.API_URL).pipe(
       tap((allServices: any) => console.log(`${allServices}`)),
       catchError(error => of([])),
     );
   }
 
-  getServicesById(id: string):Observable<Services | any>{
+  getServicesById(id: string): Observable<Services | any> {
     return this.http.get<Services>(`${this.API_URL}/${id}`).pipe(
       tap(selectedService => console.log(`${selectedService}`)),
       catchError(error => of(new Services())),
     );
   }
-   
-  updateService(id: string, body:object): Observable<Services>{
+
+  updateService(id: string, body: object): Observable<Services> {
     return this.http.patch<Services>(`${this.API_URL}/${id}`, body).pipe(
       tap(updatedRecord => console.log(`${updatedRecord}`)),
       catchError(error => of(new Services())),
     );
   }
 
-  deletedService(id:string):Observable<Services>{
+  deletedService(id: string): Observable<Services> {
     return this.http.delete<Services>(`${this.API_URL}/${id}`).pipe(
-      tap( deletedService => console.log(`${deletedService}`)),
-      catchError( error => of())
+      tap(deletedService => console.log(`${deletedService}`)),
+      catchError(error => of())
     );
   }
 }
--- src/app/Services/product.service.ts
+++ src/app/Services/product.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Products } from '../Models/product';
+import { Products } from '../models/product';
 import { environment } from 'src/environments/environment';
 import { Observable, of, catchError, map, tap } from 'rxjs';
 
@@ -13,38 +13,38 @@ export class ProductService {
 
   constructor(private http: HttpClient) { }
 
-  createproduct(data:object):Observable<Products>{
+  createproduct(data: object): Observable<Products> {
     return this.http.post<Products>(`${this.API_URL}`, data).pipe(
-      tap( newProduct => console.log(`${newProduct}`)),
-      catchError( error => of())
+      tap(newProduct => console.log(`${newProduct}`)),
+      catchError(error => of())
     );
   }
 
-  getProducts(): Observable<Products[]>{
+  getProducts(): Observable<Products[]> {
     return this.http.get<Products[]>(this.API_URL).pipe(
       tap((allProducts: any) => console.log(`${allProducts}`)),
       catchError(error => of([])),
     );
   }
 
-  getProductById(id: string):Observable<Products | any>{
+  getProductById(id: string): Observable<Products | any> {
     return this.http.get<Products>(`${this.API_URL}/${id}`).pipe(
       tap(selectedproduct => console.log(`${selectedproduct}`)),
       catchError(error => of(new Products())),
     );
   }
-   
-  updateProduct(id: string, body:object): Observable<Products>{
+
+  updateProduct(id: string, body: object): Observable<Products> {
     return this.http.patch<Products>(`${this.API_URL}/${id}`, body).pipe(
       tap(updatedRecord => console.log(`${updatedRecord}`)),
       catchError(error => of(new Products())),
     );
   }
 
-  deleteProduct(id:string):Observable<Products>{
+  deleteProduct(id: string): Observable<Products> {
     return this.http.delete<Products>(`${this.API_URL}/${id}`).pipe(
-      tap( deletedProduct => console.log(`${deletedProduct}`)),
-      catchError( error => of())
+      tap(deletedProduct => console.log(`${deletedProduct}`)),
+      catchError(error => of())
     );
   }
 }
