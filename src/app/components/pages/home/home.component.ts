c04b6c43ed91a7782f8ba5a20b72370e467c5a93
Tyrese <jrackfinn@gmail.com>
2022-09-15 13:31:14 -0500

    homepage updated

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PlumbingServicesService } from 'src/app/services/plumbing-services.service';
+import { ProductService } from 'src/app/services/product.service';
+import { ContactService } from 'src/app/services/contact.service';
+import { Products } from 'src/app/models/product';
+import { Services } from 'src/app/models/service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  services: Services[] = []
+  products: Products[] = []
+
+  constructor(private plumServices: PlumbingServicesService, private productService: ProductService, private contactService: ContactService) { }
+
+  getPlumbingServices() {
+    this.plumServices.getServices().subscribe(plumbingService => {
+      this.services = plumbingService
+      console.log(this.services);
+
+    })
+  }
 
   ngOnInit(): void {
+    this.getPlumbingServices()
   }
 
 }
